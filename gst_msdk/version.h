1be48074c59def263b1c1717a3576a4cead149af
Ishmael Visayana Sameen <ishmael1985@gmail.com>
2018-03-20 06:18:59 +0100

    Enable 32-bit and 64-bit debug and release builds

--- gst_msdk/version.h
+++ gst_msdk/version.h
@@ -2,13 +2,13 @@
 #define PACKAGE "gstmfx"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "GStreamer Media SDK"
+#define PACKAGE_NAME "GST-MFX"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer Media SDK 1.4.0"
+#define PACKAGE_STRING "GST-MFX 2.0.2"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "gstreamer-msdk"
+#define PACKAGE_TARNAME "gst-mfx"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.4.0"
+#define PACKAGE_VERSION "2.0.2"
