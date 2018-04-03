d5b1e1ba8f3b42ada08ba6a60dae770b7e5182b2
Ishmael Sameen <ishmael1985@gmail.com>
2018-04-03 19:07:08 +0200

    Update from latest master April 3, 2018

--- gst_msdk/version.h
+++ gst_msdk/version.h
@@ -5,10 +5,10 @@
 #define PACKAGE_NAME "GST-MFX"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GST-MFX 2.0.2"
+#define PACKAGE_STRING "GST-MFX 2.1.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gst-mfx"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.2"
+#define PACKAGE_VERSION "2.1.0"
